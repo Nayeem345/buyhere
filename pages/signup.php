bd4e2b1cc82469bb8e452a78e70798451e52beca
nayeem345 <shakilap4444@gmail.com>
2022-06-05 15:49:45 +0600

    signup kaj

--- pages/signup.php
+++ pages/signup.php
@@ -1,3 +1,49 @@
+
+<?php 
+
+include '../database/db.php';
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['pass'];
+$caddress = $_POST['caddress'];
+$paddress = $_POST['paddress'];
+$oadress = $_POST['oadress'];
+$phoneno = $_POST['phoneno'];
+$dphoneno = $_POST['dphoneno'];
+$photo = $_POST['photo'];
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+else{
+  
+  $sql = "INSERT INTO login(username,password) VALUES('$u','$p') ";
+  $result = $conn->query($sql);
+
+  // die(var_dump($sql));
+
+  if ($result) {
+
+    header("location: login_desg.php");
+  } else {
+    echo "0 results";
+  }
+
+
+
+
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,17 +71,17 @@
         <form action="" method="">
 
             <h3>Sign Up </h3>
-            <input type="email" name="" id="" class="box" placeholder="Enter your email">
-            <input type="password" name="" id="" class="box" placeholder="Enter your password">
-            <input type="text" name="" id="" class="box" placeholder="Enter your username">
+            <input type="email" name="email" id="" class="box" placeholder="Enter your email">
+            <input type="password" name="pass" id="" class="box" placeholder="Enter your password">
+            <input type="text" name="username" id="" class="box" placeholder="Enter your username">
             
-            <input type="text" name="" id="" class="box" placeholder="Enter your current address">
-            <input type="text" name="" id="" class="box" placeholder="Enter your permanent address">
-            <input type="text" name="" id="" class="box" placeholder="Enter your office address">
-            <input type="text" name="" id="" class="box" placeholder="Enter your phone number">
-            <input type="text" name="" id="" class="box" placeholder="Enter your delivery phone number">
+            <input type="text" name="caddress" id="" class="box" placeholder="Enter your current address">
+            <input type="text" name="paddress" id="" class="box" placeholder="Enter your permanent address">
+            <input type="text" name="oaddress" id="" class="box" placeholder="Enter your office address">
+            <input type="text" name="phoneno" id="" class="box" placeholder="Enter your phone number">
+            <input type="text" name="dphoneno" id="" class="box" placeholder="Enter your delivery phone number">
             <label for="file">Upload Your  Photo</label>
-            <input type="file" name="" id="">
+            <input type="file" name="photo" id="">
             <input type="submit" value="Signup now" class="btn">
            
             
